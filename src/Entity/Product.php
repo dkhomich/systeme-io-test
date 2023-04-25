9ddc836cd65d1d9fa8299e1ff3a09d88bface7ff
Dmitrii Khomich <37340156+dkhomich@users.noreply.github.com>
2023-04-25 10:29:20 +0300

    Add product price assertion

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ProductRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
 class Product
@@ -17,6 +18,7 @@ class Product
     private ?string $name = null;
 
     #[ORM\Column]
+    #[Assert\Positive]
     private ?float $price = null;
 
     public function __toString(): string
